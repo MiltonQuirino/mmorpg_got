9806e1111e8bd5c15137adba6fef5cf03e60471e
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-20 19:10:44 -0300

    add control permission

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,14 @@
 module.exports.jogo = function(application, req, res){
-  	res.render('jogo');
+		if(req.session.autorizado){
+			res.render('jogo');
+		}else{
+			res.send('Precisa estar logado para acessar a pagina'); 
+		}
+}
+
+module.exports.sair = function(application, req, res){
+		
+		req.session.destroy(function(erro){
+			res.render('index', {validacao:{}});
+		});  	
 }
